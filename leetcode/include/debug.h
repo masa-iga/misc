31003200cd64689736372fde2f004a43c0235f04
Masanori Igarashi <masanori.igarashi1@gmail.com>
2022-06-21 09:39:20 +0900

    added debug.h

---
+++ leetcode/include/debug.h
@@ -0,0 +1,17 @@
+#define MY_ASSERT(exp) \
+{ \
+    if (!(exp)) \
+    { \
+        printf("Line %d. Assertion failed.\n", __LINE__); \
+        assert(false); \
+    } \
+}
+
+#define MY_ASSERT2(ans, exp) \
+{ \
+    if (ans != exp) \
+    { \
+        printf("Line %d. Assertion failed. (ans %d, exp %d)\n", __LINE__, ans, exp); \
+        assert(false); \
+    } \
+}
