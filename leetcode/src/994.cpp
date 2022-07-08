cea6095cc281587ec0bdc06a8dda53078bdd7d0b
Masanori Igarashi <masanori.igarashi1@gmail.com>
2022-07-08 10:38:23 +0900

    updated 994.cpp

--- leetcode/src/994.cpp
+++ leetcode/src/994.cpp
@@ -23,50 +23,54 @@ public:
 
         queue<pair<int32_t, int32_t>> q;
 
+        size_t numOfFresh = 0;
+
         for (int32_t y = 0; y < colNum; ++y)
+        {
             for (int32_t x = 0; x < rowNum; ++x)
+            {
                 if (grid.at(y).at(x) == Cell::kRotten)
                     q.emplace(make_pair(x, y));
 
-        constexpr array<pair<int32_t, int32_t>, 4> dir = {make_pair(-1, 0), make_pair(1, 0), make_pair(0, -1), make_pair(0, 1)};
-        int32_t elapse = 0;
-        queue<pair<int32_t, int32_t>> nq;
-
-        while (!q.empty())
-        {
-            auto [x, y] = q.front();
-            q.pop();
-
-            for (const auto& d : dir)
-            {
-                const int32_t nx = x + d.first;
-                const int32_t ny = y + d.second;
-
-                const bool bOutOfRange = nx < 0 || ny < 0 || rowNum <= nx || colNum <= ny;
-
-                if (bOutOfRange || grid.at(ny).at(nx) != kFresh)
-                    continue;
-                
-                grid.at(ny).at(nx) = kRotten;
-                nq.emplace(make_pair(nx, ny));
-            }
-
-            if (q.empty())
-            {
-                if (nq.empty())
-                    break;
-
-                queue<pair<int32_t, int32_t>> temp = std::move(q);
-                q = std::move(nq);
-                nq = std::move(temp);
-                elapse++;
+                if (grid.at(y).at(x) == Cell::kFresh)
+                    ++numOfFresh;
             }
         }
 
-        for (int32_t y = 0; y < colNum; ++y)
-            for (int32_t x = 0; x < rowNum; ++x)
-                if (grid.at(y).at(x) == Cell::kFresh)
-                    return -1;
+        constexpr array<pair<int32_t, int32_t>, 4> dir = {make_pair(-1, 0), make_pair(1, 0), make_pair(0, -1), make_pair(0, 1)};
+        int32_t elapse = 0;
+
+        while (!q.empty() && numOfFresh > 0)
+        {
+            const size_t qLen = q.size();
+
+            for (size_t i = 0; i < qLen; ++i)
+            {
+                auto [x, y] = q.front();
+                q.pop();
+
+                for (const auto &d : dir)
+                {
+                    const int32_t nx = x + d.first;
+                    const int32_t ny = y + d.second;
+
+                    const bool bOutOfRange = nx < 0 || ny < 0 || rowNum <= nx || colNum <= ny;
+
+                    if (bOutOfRange || grid.at(ny).at(nx) != kFresh)
+                        continue;
+
+                    grid.at(ny).at(nx) = kRotten;
+                    q.emplace(make_pair(nx, ny));
+
+                    --numOfFresh;
+                }
+            }
+
+            elapse++;
+        }
+
+        if (numOfFresh > 0)
+            return -1;
 
         return elapse;
     }
@@ -124,9 +128,10 @@ int32_t main (int32_t argc, char* argv[])
             {{{2, 1, 1}, {1, 1, 0}, {0, 1, 1}}, 4},
         };
 
-        for (auto& inp : inputs )
+        for (const auto& inp : inputs )
         {
-            ans = solution.orangesRotting(inp.grid);
+            auto i = inp.grid;
+            ans = solution.orangesRotting(i);
             MY_ASSERT(ans == inp.exp);
         }
     }
