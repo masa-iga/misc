32078100bbd71ad85f14c335b958f038e49568fd
Masanori Igarashi <masanori.igarashi1@gmail.com>
2022-08-14 08:20:00 +0900

    updated 310.cpp

--- leetcode/src/310.cpp
+++ leetcode/src/310.cpp
@@ -3,6 +3,8 @@
 #include <cassert>
 #include <vector>
 #include <algorithm>
+#include <queue>
+#include <set>
 
 using namespace std;
 
@@ -18,57 +20,47 @@ using namespace std;
 
 class Solution {
 public:
-    int32_t dfs(int32_t node, int32_t depth, vector<bool> bVisited, const vector<vector<int32_t>>& graph)
-    {
-        int32_t maxDepth = depth;
-
-        for (int32_t edge : graph.at(node))
-        {
-            if (bVisited.at(edge))
-                continue;
-            
-            bVisited.at(edge) = true;
-            const int32_t d = dfs(edge, depth + 1, bVisited, graph);
-            maxDepth = std::max(d, maxDepth);
-        }
-
-        return maxDepth;
-    }
-
     vector<int> findMinHeightTrees(int n, vector<vector<int>> &edges)
     {
-        vector<vector<int32_t>> graph(n);
-
-        for (const vector<int32_t> e : edges)
+        vector<set<int32_t>> graph(n);
         {
-            assert(e.size() == 2);
-            assert(e.at(0) != e.at(1));
-            graph.at(e.at(0)).emplace_back(e.at(1));
-            graph.at(e.at(1)).emplace_back(e.at(0));
-        }
-
-        vector<int32_t> mhts(n, INT32_MAX);
-
-        for (int32_t i = 0; i < n; ++i)
-        {
-            vector<bool> bVisited(n, false);
-            bVisited.at(i) = true;
-
-            mhts.at(i) = dfs(i, 0, bVisited, graph);
-        }
-
-        vector<int32_t> ans;
-        const int32_t min = *min_element(mhts.begin(), mhts.end());
-
-        for (int32_t i = 0; i < n; ++i)
-        {
-            if (mhts.at(i) == min)
+            for (const vector<int32_t>& edge : edges)
             {
-                ans.emplace_back(i);
+                graph.at(edge.at(0)).emplace(edge.at(1));
+                graph.at(edge.at(1)).emplace(edge.at(0));
             }
         }
 
-        return ans;
+        vector<int32_t> leaves;
+        {
+            for (int32_t i = 0; i < n; ++i)
+                if (graph.at(i).size() <= 1)
+                    leaves.emplace_back(i);
+        }
+
+        while (n > 2)
+        {
+            n -= leaves.size();
+
+            vector<int32_t> nextLeaves;
+
+            for (const int32_t leave : leaves)
+            {
+                assert(graph.at(leave).size() == 1);
+                const int32_t nextNode = *graph.at(leave).begin();
+                graph.at(nextNode).erase(leave);
+
+                if (graph.at(nextNode).size() == 1)
+                {
+                    nextLeaves.emplace_back(nextNode);
+                }
+            }
+
+            assert(nextLeaves.size() != 0);
+            leaves = std::move(nextLeaves);
+        }
+
+        return leaves;
     }
 };
 
@@ -78,20 +70,70 @@ int32_t main(int32_t argc, char* argv[])
     int32_t n = 0;
     vector<vector<int32_t>> edges;
     vector<int32_t> ans, exp;
+
     {
-        n = 4;
-        edges = {{1, 0}, {1, 2}, {1, 3}};
+        n = 1;
+        edges = {};
         ans = solution.findMinHeightTrees(n, edges);
-        exp = {1};
+        exp = {0};
         assert(ans == exp);
     }
 
     {
+        n = 2;
+
+        edges = {{0, 1}};
+        exp = {0, 1};
+        ans = solution.findMinHeightTrees(n, edges); assert(ans == exp);
+
+        edges = {{1, 0}};
+        exp = {0, 1};
+        ans = solution.findMinHeightTrees(n, edges); assert(ans == exp);
+    }
+
+    {
+        n = 3;
+
+        edges = {{0, 1}, {0, 2}};
+        exp = {0};
+        ans = solution.findMinHeightTrees(n, edges); assert(ans == exp);
+
+        edges = {{0, 1}, {1, 2}};
+        exp = {1};
+        ans = solution.findMinHeightTrees(n, edges); assert(ans == exp);
+
+        edges = {{0, 2}, {1, 2}};
+        exp = {2};
+        ans = solution.findMinHeightTrees(n, edges); assert(ans == exp);
+    }
+
+    {
+        n = 4;
+
+        edges = {{1, 0}, {1, 2}, {1, 3}};
+        exp = {1};
+        ans = solution.findMinHeightTrees(n, edges); assert(ans == exp);
+    }
+
+    {
         n = 6;
+
         edges = {{3,0},{3,1},{3,2},{3,4},{5,4}};
-        ans = solution.findMinHeightTrees(n, edges);
         exp = {3, 4};
-        assert(ans == exp);
+        ans = solution.findMinHeightTrees(n, edges); assert(ans == exp);
+
+        edges = {{0,1},{0,2},{0,3},{3,4},{4,5}};
+        exp = {3};
+        ans = solution.findMinHeightTrees(n, edges); assert(ans == exp);
+    }
+
+    {
+        // TODO: debug
+        n = 7;
+
+        edges = {{0,1},{1,2},{1,3},{2,4},{3,5},{4,6}};
+        exp = {1, 2};
+        ans = solution.findMinHeightTrees(n, edges); assert(ans == exp);
     }
 
     return 0;
