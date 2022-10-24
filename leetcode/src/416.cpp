c3aa4fe835d1187a921f8bfb504da8b568e659a1
Masanori Igarashi <masanori.igarashi1@gmail.com>
2022-10-24 10:08:23 +0900

    updated 416.cpp

--- leetcode/src/416.cpp
+++ leetcode/src/416.cpp
@@ -28,41 +28,39 @@ do { \
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        const int32_t sum = accumulate(nums.begin(), nums.end(), 0);
+        int32_t sum = accumulate(nums.begin(), nums.end(), 0);
 
         if (sum % 2 != 0)
             return false;
+        
+        sum /= 2;
 
-        vector<vector<int32_t>> cache(nums.size(), vector<int32_t>(sum/2 + 1, kInvalid));
+        vector<vector<int32_t>> cache(nums.size() + 1, vector<int32_t>(sum + 1, kInvalid));
 
-        return dp(nums, cache, nums.size() - 1, sum / 2);
+        for (int32_t i = 1; i < nums.size(); ++i)
+            cache.at(i).at(0) = true;
+        
+        for (int32_t j = 1; j < sum + 1; ++j)
+            cache.at(0).at(j) = false;
+
+        for (int32_t i = 1; i < nums.size() + 1; ++i)
+        {
+            for (int32_t j = 1; j < sum + 1; ++j)
+            {
+                cache.at(i).at(j) = cache.at(i - 1).at(j);
+
+                if (j >= nums.at(i - 1))
+                {
+                    cache.at(i).at(j) |= cache.at(i - 1).at(j - nums.at(i - 1));
+                }
+            }
+        }
+
+        return cache.at(nums.size()).at(sum);
     }
 
 private:
     static constexpr int32_t kInvalid = INT32_MIN;
-
-    bool dp(const vector<int32_t>& nums, vector<vector<int32_t>>& cache, int32_t i, int32_t j)
-    {
-        ASSERT(0 <= i && i < cache.size());
-        ASSERT(j < static_cast<int32_t>(cache.at(0).size()));
-
-        if (j < 0)
-            return false;
-
-        if (cache.at(i).at(j) != kInvalid)
-            return cache.at(i).at(j);
-
-        if (i == 0)
-            return (nums.at(0) == j);
-
-        if (nums.at(i) == j)
-            return true;
-
-        const bool ret = dp(nums, cache, i - 1, j) | dp(nums, cache, i - 1, j - nums.at(i));
-        cache.at(i).at(j) = ret;
-
-        return ret;
-    }
 };
 
 int32_t main(int32_t argc, char* argv[])
