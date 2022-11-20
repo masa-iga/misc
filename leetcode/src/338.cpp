c695b0ef7d455bb4825abaac1fbf8eadbfd16264
Masanori Igarashi <masanori.igarashi1@gmail.com>
2022-11-20 10:45:16 +0900

    improved 338.cpp

--- leetcode/src/338.cpp
+++ leetcode/src/338.cpp
@@ -20,32 +20,32 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int32_t> ans;
+        ans.reserve(n + 1);
 
-        for (int32_t i = 0; i <= n; ++i)
+        int32_t power = 0;
+
+        for (int32_t i = 0; i < n + 1; ++i)
         {
-            ans.emplace_back(countBit(i));
+            if (i < 2)
+            {
+                ans.emplace_back(i & 0x1);
+                continue;
+            }
+
+            if (isPowerOfTwo(i))
+                ++power;
+            
+            const int32_t idx = i & ~(1 << power);
+            ans.emplace_back(ans[idx] + 1);
         }
 
         return ans;
     }
 
 private:
-    int32_t countBit(int32_t n)
+    inline bool isPowerOfTwo(int32_t n)
     {
-        int32_t num = n;
-        int32_t count = 0;
-
-        while (num != 0)
-        {
-            if (num & 0x1)
-                ++count;
-            
-            num = num >> 1;
-        }
-
-        DPRINTF("%d: %d\n", n, count);
-
-        return count;
+        return (n & n - 1) == 0;
     }
 };
 
